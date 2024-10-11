2c1cd35daaea2488d0b9770dc12fe538c3d6708e
Fabricio Rodrigues <fabricio12nastacio@gmail.com>
2024-10-11 15:27:05 -0300

    Remove a barra lateral

--- frontend/src/components/ComponentFooter.jsx
+++ frontend/src/components/ComponentFooter.jsx
@@ -8,21 +8,28 @@ import {
 function Footer() {
   return (
     <footer id="Contact-footer">
-      <div className="Footer-links">
-        <a href="https://github.com/FabricioAnastacio" target="_blank" rel="noreferrer">
-          <img src={ github } alt="Github Link" />
-        </a>
-        <a href="https://www.linkedin.com/in/far-dev/" target="_blank" rel="noreferrer">
-          <img src={ linkedin } alt="Linkedin Link" />
-        </a>
-        <a href="https://www.instagram.com/fabricio.rodrigues_2.0_/" target="_blank" rel="noreferrer">
-          <img src={ instagram } alt="Instagram Link" />
-        </a>
+      <div className="References">
+        <ul>
+          <li>Fonte: SUSE</li>
+        </ul>
       </div>
-      <ul>
-        <li>Fonte SUSE</li>
-      </ul>
-      <h2>Obrigado por acessar minha humilde pagina</h2>
+      <div className="Footer-links">
+        <section>
+          <a href="https://github.com/FabricioAnastacio" target="_blank" rel="noreferrer">
+            <img src={ github } alt="Github Link" />
+          </a>
+          <a href="https://www.linkedin.com/in/far-dev/" target="_blank" rel="noreferrer">
+            <img src={ linkedin } alt="Linkedin Link" />
+          </a>
+          <a href="https://www.instagram.com/fabricio.rodrigues_2.0_/" target="_blank" rel="noreferrer">
+            <img src={ instagram } alt="Instagram Link" />
+          </a>
+        </section>
+        <h2>Obrigado por acessar minha humilde pagina</h2>
+      </div>
+      <div className="Autor">
+        <p>Fabrício&reg; 2024</p>
+      </div>
     </footer>
   );
 }
