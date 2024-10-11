a9f4b8afc08a4f8eddbd2b5304c2fd46a93e4eea
Fabricio Rodrigues <fabricio12nastacio@gmail.com>
2024-10-11 14:19:57 -0300

    Deve-se corrigir o envio de e-mail

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -1,21 +1,67 @@
+import { useState } from 'react';
 import '../styles/Contact.css';
 
 function Contact() {
+  const [email, setEmail] = useState('');
+  const [name, setName] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleChenge = ({ target }) => {
+    switch (target.name) {
+    case 'e-mail':
+      setEmail(target.value);
+      break;
+    case 'name':
+      setName(target.value);
+      break;
+    default:
+      setMessage(target.value);
+    }
+  };
+
   return (
     <section className="Contact">
-      <form action="">
+      <form action="https://formsubmit.co/fabricio12nastacio@gmail.com​" method="POST">
         <label>
           Seu e-mail:
-          <input type="text" />
+          <input
+            name="e-mail"
+            value={ email }
+            type="e-mail"
+            required
+            onChange={ handleChenge }
+          />
         </label>
         <label>
-          Assunto:
-          <input type="text" />
+          Seu nome:
+          <input
+            name="name"
+            value={ name }
+            type="text"
+            required
+            onChange={ handleChenge }
+          />
         </label>
         <label>
           Menssagem:
-          <textarea type="text" />
+          <textarea
+            name="message"
+            value={ message }
+            type="text"
+            required
+            onChange={ handleChenge }
+          />
         </label>
+        <input
+          type="hidden"
+          name="_subject"
+          value="Novo contato!"
+        />
+        <input
+          type="text"
+          name="_honey"
+          style={ { display: 'none' } }
+        />
         <button type="submit">Enviar</button>
       </form>
     </section>
