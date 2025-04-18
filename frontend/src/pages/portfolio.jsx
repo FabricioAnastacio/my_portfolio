b2e01f83db5376f3ac2363530481d4aaeca2da87
Fabricio <fabricio12nastacio@gmail.com>
2025-04-18 14:17:21 -0300

    Header mobile concluido (Alterações devem ser feitas na organizção dos arquivos)

--- frontend/src/pages/portfolio.jsx
+++ frontend/src/pages/portfolio.jsx
@@ -4,6 +4,7 @@ import Main from '../desktop/components/ComponentMain';
 import Header from '../desktop/components/ComponentHeader';
 import AppContext from '../context/AppContext';
 import Footer from '../desktop/components/ComponentFooter';
+import HeaderMobile from '../mobile/components/ComponentHeader';
 
 function Portfolio() {
   const { modeThame } = useContext(AppContext);
@@ -19,20 +20,23 @@ function Portfolio() {
   );
 
   const mobileAPP = (
-    <h1
-      style={ {
-        textAlign: 'center',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        height: '100vh',
-        color: 'red',
-      } }
-    >
-      Desculpe
-      <br />
-      A versão Mobile esta em produção!
-    </h1>
+    <div>
+      <HeaderMobile />
+      <h1
+        style={ {
+          textAlign: 'center',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          height: '100vh',
+          color: 'red',
+        } }
+      >
+        Desculpe
+        <br />
+        A versão Mobile esta em produção!
+      </h1>
+    </div>
   );
 
   return width > maxWidth ? desktopAPP : mobileAPP;
