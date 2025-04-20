42a050ab21de2ac28cb9544455f93446b767b45e
Fabricio <fabricio12nastacio@gmail.com>
2025-04-20 16:33:30 -0300

    Adiciona no rodape a verdade dita por Cristo

--- frontend/src/components/common/FooterAutor.jsx
+++ frontend/src/components/common/FooterAutor.jsx
@@ -3,9 +3,15 @@ import PropTypes from 'prop-types';
 function FooterAutor({ platform }) {
   return (
     <div className={ `Autor-${platform}` }>
-      <p>Fabrício&reg; 2024</p>
-      <p>|</p>
-      <p>Obrigado por acessar.</p>
+      <section>
+        <p>FabrícioA.R.&reg; 2024</p>
+        <p>|</p>
+        <p>Obrigado por acessar.</p>
+      </section>
+      <div className={ `Versiculo-${platform}` }>
+        <p>&#34;Conheceis a verdade, e a verdade vos libertará&#34;</p>
+        <p>João 8:32</p>
+      </div>
     </div>
   );
 }
